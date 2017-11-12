a98ddb276e515b704e8e759477134ca219044b7b
Diego Sieg <diegosieg@gmail.com>
2017-11-12 20:05:55 +1300

    More updates to stats;

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pie} from 'react-chartjs-2';
+import {Pie, HorizontalBar} from 'react-chartjs-2';
 //import PropTypes from 'prop-types';
 
 function Stats(props) {
@@ -20,9 +20,9 @@ function Stats(props) {
 
   const dataBooks = {
     labels: [
-      'Currently reading',
-      'Want to read',
-      'Read'
+      'Currently being read',
+      'To be read',
+      'Already read'
     ],
     datasets: [{
       data: [statsBooks[0].currentlyReading.length, statsBooks[0].wantToRead.length, statsBooks[0].read.length],
@@ -41,35 +41,33 @@ function Stats(props) {
 
   const dataPages = {
     labels: [
-      'Pages currently being reading',
-      'Pages to be to read',
-      'Pages already read'
+      'Currently being read',
+      'To be read',
+      'Already read'
     ],
-    datasets: [{
-      data: [pagesBooksCurrently, pagesBooksWanted, pagesBooksRead],
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ],
-      hoverBackgroundColor: [
-        '#D93D5E',
-        '#107CC5',
-        '#D9A830'
-      ]
-    }]
+  datasets: [
+    {
+      label: 'approximate number of pages',
+      backgroundColor: '#36A2EB',
+      borderColor: '#107CC5',
+      borderWidth: 1,
+      hoverBackgroundColor: '#107CC5',
+      hoverBorderColor: 'rgba(255,255,255,1)',
+      data: [pagesBooksCurrently, pagesBooksWanted, pagesBooksRead]
+    }
+  ]
   };
 
   return (
       <div className="shelves-stats-box">
         <h2 className="shelves-stats-title">Shelves Stats</h2>
         <div className="shelves-stats-chart">
-          <h4 className="shelves-stats-chart-title">Number of books</h4>
-          <Pie data={dataBooks} />
+          <h4 className="shelves-stats-chart-title">Number of pages</h4>
+          <HorizontalBar data={dataPages} />
         </div>
         <div className="shelves-stats-chart">
-          <h4 className="shelves-stats-chart-title">Number of pages</h4>
-          <Pie data={dataPages} />
+          <h4 className="shelves-stats-chart-title">Number of books</h4>
+          <Pie data={dataBooks} />
         </div>
       </div>
   );
